ec07d75f01b3dd257e2abba9deee3287e04cee0b
DeenoDev <deyanoklaasen@outlook.com>
2023-08-04 23:30:02 +0200

    Used intersectionObserver callback function to conditionally toggle class.

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const scrollWatcher = document.createElement('div');
 
 scrollWatcher.setAttribute('data-scroll-watcher', 'true');
 primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+    primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting);
+});
+
+navObserver.observe(scrollWatcher);
