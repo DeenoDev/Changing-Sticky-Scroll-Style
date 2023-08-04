5d9317ecb7f062df4d17620139de01a8b7760e3d
DeenoDev <deyanoklaasen@outlook.com>
2023-08-04 23:35:13 +0200

    Project completed and ready for push to main.

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ primaryHeader.before(scrollWatcher);
 
 const navObserver = new IntersectionObserver((entries) => {
     primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting);
-});
+}, {rootMargin: "200px 0px 0px 0px"});
 
 navObserver.observe(scrollWatcher);
